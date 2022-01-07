665e079d32518286cd2ead2eb98b21fe770e0bdd
Suciu Andrei <andreisuciu146@gmail.com>
2022-01-07 12:39:05 +0200

    modify user

--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -3,16 +3,38 @@ package com.example.domain;
 import java.util.Objects;
 
 public class User extends Entity<Integer> {
-    private String firstName, lastName;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     /**
      * Contstructor for the user class
      * @param firstName string representing the name of the user (0&lt;name.length)
      * @param lastName  string representing the surname of the user(0&lt;surname.length)
+    //* @param username
      */
-    public User(String firstName, String lastName) {
+    public User(String username, String firstName, String lastName,String password) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     /**
@@ -78,3 +100,4 @@ public class User extends Entity<Integer> {
     }
 
 }
+
